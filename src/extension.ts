5b698ce203368eeec348f1c49559b1c0a16d4dd8
Matias Scharager <matias.scharager@gmail.com>
2024-09-24 16:08:34 -0400

    call istari

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,50 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { ChildProcess, spawn } from 'child_process';
 import * as vscode from 'vscode';
+
+class IstariTerminal {
+	editor: vscode.TextEditor;
+	terminal: ChildProcess;
+	constructor(editor: vscode.TextEditor) {
+		this.editor = editor;
+
+		this.terminal = spawn("sml", ["@SMLload=/home/pi314mm/Desktop/istari/ui/bin/istarisrv-heapimg.amd64-linux"])
+		//this.terminal = vscode.window.createTerminal("istari: " + editor.document.fileName);
+		//this.terminal.sendText("sml @SMLload=/home/pi314mm/Desktop/istari/ui/bin/istarisrv-heapimg.amd64-linux", true);
+		//this.terminal.sendText("\x06\n");
+		//const wordAtCurorRange = new vscode.Range(new vscode.Position(0,0), editor.selection.active)
+		//terminal.sendText(editor.document.getText(wordAtCurorRange));
+		//terminal.sendText("\x05\n");
+		this.terminal.stdin?.write("\x06\n");
+		this.terminal.stdout?.on('data', (message) => {
+				console.log(`${message}`);
+			}
+		)
+	}
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "istari" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('istari.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from istari!');
+
+		const editor = vscode.window.activeTextEditor ;
+		if (editor){
+			const istari = new IstariTerminal(editor);
+
+			
+			
+			const wordAtCurorRange = new vscode.Range(new vscode.Position(0,0), editor.selection.active)
+			istari.terminal.stdin?.write(editor.document.getText(wordAtCurorRange));
+			istari.terminal.stdin?.write("\x05\n");
+			
+
+		} else {
+			console.log('No text window is active');
+		}
 	});
 
 	context.subscriptions.push(disposable);
